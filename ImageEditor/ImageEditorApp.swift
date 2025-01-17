26ca50bf637097df44bd2db52fe37503d41c650c
PowerUpSkills <douloumis@gmail.com>
2025-01-17 03:35:34 +0100

    Initial Commit

---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by douloumis on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
